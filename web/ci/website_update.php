a614a79af18a672aaba3f04c2e168ab931bab853
Sam Tupy <webmaster@samtupy.com>
2024-05-08 14:17:49 -0500

    website_update.php should now clean blog, ci and docs directory during update so as to not leave old files around

--- web/ci/website_update.php
+++ web/ci/website_update.php
@@ -1,4 +1,9 @@
 <?php
 require "auth.php";
+// Remove some common directories that we know will be contained in _site.tar.gz to account for any deletes/renames in the extracted archive. If we find we keep updating this, we can consider looping through directories and deleting the ones that contain a specific file.
+echo `rm -r ../blog`;
+echo `rm -r ../docs`;
+echo `rm -r *`; // We even delete ourself!
+// Now load the updated site and undo anything we just deleted.
 echo `tar -xzf ../../ci/_site.tar.gz -C ..`;
 ?>
