1a0761be52986aaa6b772bbfeb5803b46b931bcf
TheQuinbox <thequinbox@gmail.com>
2024-05-30 00:39:08 -0600

    Add beginnings of make_dmg.py

--- install/make_dmg.py
+++ install/make_dmg.py
@@ -1,5 +1,21 @@
+import glob
+import os
 from pathlib import Path
+import shutil
 import subprocess
+import sys
+
+# This function basically performs `chmod +x`, but as a Python function. Thanks to https://stackoverflow.com/questions/12791997/
+def make_executable(path):
+	mode = os.stat(path).st_mode
+	mode |= (mode & 0o444) >> 2    # copy R bits to X
+	os.chmod(path, mode)
+
+def make_app_bundle(bundle_name, release_path):
+	bundle_basename = f"{bundle_name}/Contents"
+	shutil.copytree(release_path, bundle_basename + "/MacOS")
+	os.mkdir(bundle_basename + "/Resources")
+	os.rename(bundle_basename + "/MacOS/stub", bundle_basename + "/Resources/stub")
 
 def make_dmg(src_dir, filename):
 	subprocess.check_call(["hdiutil", "create", "-srcfolder", src_dir, filename])
@@ -7,4 +23,7 @@ def make_dmg(src_dir, filename):
 def get_version_info():
 	return Path("../version").read_text().replace("-", "_")
 
-make_dmg("../release", "nvgt_" + get_version_info() + ".dmg")
+relpath = "../release"
+if len(sys.argv) > 1:
+	relpath = " ".join(sys.argv[1:])
+make_app_bundle("nvgt.app", relpath)
