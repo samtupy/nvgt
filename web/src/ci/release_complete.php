8c2151c25532764b96ff9c7a3e8f339813c3f293
Sam Tupy <webmaster@samtupy.com>
2024-06-03 14:08:28 -0500

    slightly improve formatting on downloads page

--- web/src/ci/release_complete.php
+++ web/src/ci/release_complete.php
@@ -2,5 +2,6 @@
 require "auth.php";
 // This copies a couple of release artifacts into the downloads directory.
 if (!isset($_GET["ver"])) die("missing version");
-echo system("cp ../../ci/nvgt_" . $_GET["ver"] . "* ../downloads");
+file_put_contents("../downloads/latest_version", $_GET["ver"]);
+echo system("mv -f ../../ci/nvgt_" . $_GET["ver"] . "* ../downloads");
 ?>
