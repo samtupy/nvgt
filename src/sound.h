9ccf0c9d4ca3f5bb23e4bb3629849da2fe2c4547
Ethin Probst <ethindp@pm.me>
2024-06-10 11:22:34 -0500

    Fix more mixer::set_fx bugs (#46)
    * Fixed a bug in the mixer class where an effect would fail to add on occasion but all remaining effects would be added
    
    * Finally (and hopefully) fixed all the mixer::set_fx problems.

--- src/sound.h
+++ src/sound.h
@@ -57,6 +57,7 @@ typedef struct {
 	HFX hfx;
 	unsigned int type;
 	std::string id;
+	std::string original_fx_str; // debugging only
 } mixer_effect;
 
 typedef struct {
@@ -230,6 +231,8 @@ public:
 	BOOL remove_sound(sound& s, BOOL internal = FALSE);
 	bool set_impulse_response(const std::string& response, float dry, float wet);
 	int set_fx(std::string& fx, int idx = -1);
+	void clear_all_effects();
+	CScriptArray* get_all_raw_effects();
 	BOOL set_mixer(mixer* m);
 	BOOL is_sliding();
 	BOOL is_pan_sliding();
