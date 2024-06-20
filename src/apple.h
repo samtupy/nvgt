2ef4903d4e7d95b6776ab873773fd35a9b9d962b
GruiaChiscop <166289528+GruiaChiscop@users.noreply.github.com>
2024-06-20 07:33:57 +0300

    added AVSpeechSynthesizer wrapper class (#65)

--- src/apple.h
+++ src/apple.h
@@ -12,6 +12,8 @@
 
 #pragma once
 #include <string>
+class asIScriptEngine;
+class CScriptArray;
 
 bool voice_over_is_running();
 bool voice_over_speak(const std::string& message, bool interrupt = true);
@@ -19,3 +21,39 @@ void voice_over_window_created();
 void voice_over_speech_shutdown();
 std::string apple_input_box(const std::string& title, const std::string& message, const std::string& default_value = "", bool secure = false, bool readonly = false);
 std::string apple_requested_file();
+
+// Definition for AVTTSVoice class created by Gruia Chiscop on 6/6/24.
+class AVTTSVoice {
+public:
+	AVTTSVoice();
+	AVTTSVoice(const std::string& name);
+	~AVTTSVoice();
+	void init();
+	void deinit();
+	bool speak(const std::string& text, bool interrupt);
+	bool speakWait(const std::string& text, bool interrupt);
+	bool stopSpeech();
+	bool pauseSpeech();
+	CScriptArray* getAllVoices() const;
+	CScriptArray* getVoicesByLanguage(const std::string& language) const;
+	std::string getCurrentVoice() const;
+	bool isSpeaking() const;
+	bool isPaused() const;
+	void setRate(float rate);
+	float getRate() const;
+	void setVolume(float volume);
+	float getVolume() const;
+	void setPitch(float pitch);
+	float getPitch() const;
+	void setVoiceByName(const std::string& name);
+	void setVoiceByLanguage(const std::string& language);
+	std::string getCurrentLanguage() const;
+	uint64_t getVoicesCount() const;
+	int getVoiceIndex(const std::string& name) const;
+	bool setVoiceByIndex(uint64_t index);
+	std::string getVoiceName(uint64_t index);
+private:
+	class Impl;
+	Impl* impl;
+	int RefCount;
+};
