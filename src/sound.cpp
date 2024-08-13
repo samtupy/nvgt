8e80a7b13cb8a9999df6795130287844c952bf3e
Sam Tupy <webmaster@samtupy.com>
2024-08-13 16:53:43 -0500

    work on minaudio

--- src/sound.cpp
+++ src/sound.cpp
@@ -16,21 +16,52 @@
 #include <scriptarray.h>
 #include "sound.h"
 
-// Globals, currently NVGT does not support instanciating multiple miniaudio engines or contexts.
+using namespace std;
+
+// Globals, currently NVGT does not support instanciating multiple miniaudio contexts and NVGT provides a global sound engine.
 static ma_context g_sound_context;
-static ma_engine g_sound_engine;
+static audio_engine* g_audio_engine = nullptr;
 static bool g_soundsystem_initialized = false;
 static ma_result g_soundsystem_last_error = MA_SUCCESS;
 
 bool init_sound() {
 	if (g_soundsystem_initialized) return true;
 	if ((g_soundsystem_last_error = ma_context_init(nullptr, 0, nullptr, &g_sound_context)) != MA_SUCCESS) return false;
-	if ((g_soundsystem_last_error = ma_engine_init(nullptr, &g_sound_engine)) != MA_SUCCESS) return false;
 	return true;
 }
 
+// audio device enumeration, we'll just maintain a global list of available devices, vectors of ma_device_info structures for the c++ side and CScriptArrays of device names on the Angelscript side. It is important that the data in these arrays is index aligned.
+vector<ma_device_info> g_sound_input_devices, g_sound_output_devices;
+static CScriptArray* g_sound_script_input_devices = nullptr, * g_sound_script_output_devices = nullptr;
+ma_bool32 ma_device_enum_callback(ma_context* ctx, ma_device_type type, const ma_device_info* info, void* /*user*/) {
+	string devname;
+	if (deviceType == ma_device_type_playback) {
+		g_sound_playback_devices.push_back(*info);
+		g_sound_script_playback_devices->InsertLast(&(devname = &info->name));
+	} else if (deviceType == ma_device_type_capture) {
+		g_sound_input_devices.push_back(*info);
+		g_sound_script_input_devices->InsertLast(&(devname = &info->name));
+	}
+}
+bool refresh_audio_devices() {
+	if (!init_sound()) return false;
+	g_sound_output_devices.clear();
+	g_sound_input_devices.clear();
+	g_sound_script_playback_devices->Resize(0);
+	g_sound_script_input_devices->Resize(0);
+	return (g_soundsystem_last_error = ma_context_enumerate_devices(&g_sound_context, ma_device_enum_callback, nullptr)) == MA_SUCCESS;
+}
 
-
+// Miniaudio objects must be allocated on the heap as nvgt's API introduces the concept of an uninitialized sound, which a stack based system would make more difficult to implement.
+class audio_engine_impl : public audio_engine {
+		ma_engine* engine;
+		audio_node* engine_endpoint; // Upon engine creation we'll call ma_engine_get_endpoint once so as to avoid creating more than one of our wrapper objects when our engine->get_endpoint() function is called.
+	public:
+		audio_engine_impl() {
+			// default constructor initializes a miniaudio engine ourselves.
+			ma_engine_config cfg = ma_engine_config_init();
+		}
+};
 class sound_impl : public sound {
-	ma_sound snd;
+	ma_sound* snd;
 }
--- src/sound.h
+++ src/sound.h
@@ -11,11 +11,118 @@
 */
 
 #include <miniaudio.h>
+#include "bullet3.h" // Vector3
 
 class asIScriptEngine;
+class mixer;
+class sound;
 
-class sound {
+bool init_sound();
+bool refresh_audio_devices();
+
+class audio_node {
 	
+};
+class audio_engine {
+	public:
+		enum engine_flags {
+			DURATIONS_IN_FRAMES = 1, // If set, all durations possible will expect a value in PCM frames rather than milliseconds.
+			NO_AUTO_START = 2, // if set, audio_engine::start must be called after initialization.
+			NO_DEVICE = 4 // If set, audio_engine::read() must be used to receive raw audio samples from the engine instead.
+		};
+		virtual void duplicate(); // reference counting
+		virtual void release();
+		virtual audio_node* get_endpoint();
+		virtual bool read(void* buffer, unsigned long long frame_count, unsigned long long* frames_read);
+		virtual CScriptArray* read(unsigned long long64 frame_count);
+		virtual unsigned long long get_time();
+		virtual bool set_time(unsigned long long time); // depends on DURATIONS_IN_FRAMES flag.
+		virtual unsigned long long get_time_in_frames();
+		virtual bool set_time_in_frames(unsigned long long time);
+		virtual unsigned long long get_time_in_milliseconds();
+		virtual bool set_time_in_milliseconds(unsigned long long time);
+		virtual int get_channels();
+		virtual int get_sample_rate();
+		virtual bool start(); // Begins audio playback <ma_engine_start>, only needs to be called if NO_AUTO_START flag is set in engine construction or after stop is called.
+		virtual bool stop(); // Stops audio playback.
+		virtual bool set_volume(float volume); // 0.0 to 1.0.
+		virtual float get_volume();
+		virtual bool set_gain(float db);
+		virtual float get_gain();
+		virtual unsigned int get_listener_count();
+		virtual unsigned int find_closest_listener(float x, float y, float z);
+		virtual void set_listener_position(unsigned int index, float x, float y, float z);
+		virtual Vector3 get_listener_position(unsigned int index);
+		virtual void set_listener_direction(unsigned int index, float x, float y, float z);
+		virtual Vector3 get_listener_direction(unsigned int index);
+		virtual void set_listener_velocity(unsigned int index, float x, float y, float z);
+		virtual Vector3 get_listener_velocity(unsigned int index);
+		virtual void set_listener_cone(unsigned int index, float inner_radians, float outer_radians, float outer_gain);
+		virtual void get_listener_cone(unsigned int index, float* inner_radians, float* outer_radians, float* outer_gain);
+		virtual void set_listener_world_up(unsigned int index, float x, float y, float z);
+		virtual Vector3 get_listener_world_up(unsigned int index);
+		virtual void set_listener_enabled(unsigned int index, bool enabled);
+		virtual bool get_listener_enabled(unsigned int index);
+		virtual bool play(const std::string& path, audio_node* node, unsigned int input_bus_index);
+		virtual bool play(const std::string& path, mixer* mixer = nullptr);
+		virtual mixer* new_mixer();
+		virtual sound* new_sound();
+};
+class sound {
+	public:
+		virtual audio_engine* get_engine();
+		virtual bool play();
+		virtual bool pause();
+		virtual bool pause_fade(unsigned long long length); // depends on the DURATIONS_IN_FRAMES engine flag.
+		virtual bool pause_fade_in_frames(unsigned long long frames);
+		virtual bool pause_fade_in_milliseconds(unsigned long long milliseconds);
+		virtual bool stop();
+		virtual void set_volume(float volume);
+		virtual float get_volume();
+		virtual void set_pan(float pan);
+		virtual float get_pan();
+		virtual void set_pan_mode(ma_pan_mode mode);
+		virtual ma_pan_mode get_pan_mode();
+		virtual void set_pitch(float pitch);
+		virtual float get_pitch();
+		virtual void set_spatialization_enabled(bool enabled);
+		virtual void get_spatialization_enabled();
+		virtual void set_pinned_listener(unsigned int index);
+		virtual unsigned int get_pinned_listener();
+		virtual unsigned int get_listener();
+		virtual Vector3 get_direction_to_listener();
+		virtual void set_position(float x, float y, float z);
+		virtual Vector3 get_position();
+		virtual void set_direction(float x, float y, float z);
+		virtual Vector3 get_direction();
+		virtual void set_velocity(float x, float y, float z);
+		virtual Vector3 get_velocity();
+		virtual void set_attenuation_model(ma_attenuation_model model);
+		virtual ma_attenuation_model get_attenuation_model();
+		virtual void set_positioning(ma_positioning positioning);
+		virtual ma_positioning get_positioning();
+		virtual void set_rolloff(float rolloff);
+		virtual float get_rolloff();
+		virtual void set_min_gain(float gain);
+		virtual float get_min_gain();
+		virtual void set_max_gain(float gain);
+		virtual float get_max_gain();
+		virtual void set_min_distance(float distance);
+		virtual float get_min_distance();
+		virtual void set_max_distance(float distance);
+		virtual float get_max_distance();
+		virtual void set_cone(float inner_radians, float outer_radians, float outer_gain);
+		virtual void get_cone(float* inner_radians, float* outer_radians, float* outer_gain);
+		virtual void set_doppler_factor(float factor);
+		virtual float get_doppler_factor();
+		virtual void set_directional_attenuation_factor(float factor);
+		virtual float get_directional_attenuation_factor();
+		virtual void set_fade(float start_volume, float end_volume, unsigned long long length); // depends on DURATIONS_IN_FRAMES flag in parent engine
+		virtual void set_fade_in_frames(float start_volume, float end_volume, unsigned long long frames);
+		virtual void set_fade_in_milliseconds(float start_volume, float end_volume, unsigned long long milliseconds);
+		virtual void set_timed_fade(float start_volume, float end_volume, unsigned long long length, unsigned long long absolute_time); // depends on DURATIONS_IN_FRAMES flag in parent engine
+		virtual void set_timed_fade_in_frames(float start_volume, float end_volume, unsigned long long frames, unsigned long long absolute_time_in_frames);
+		virtual void set_timed_fade_in_milliseconds(float start_volume, float end_volume, unsigned long long milliseconds, unsigned long long absolute_time_in_milliseconds);
 }
 
 void RegisterSoundsystem(asIScriptEngine* engine);
