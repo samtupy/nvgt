a040453e4d02a739f9631f330a71153abaa9863c
Harry Min Khant <118270468+harrymkt@users.noreply.github.com>
2024-09-08 11:04:45 +0630

    Js code format style

--- web/src/js/functions.js
+++ web/src/js/functions.js
@@ -1,25 +1,23 @@
-function convertdate(dateString)
-{
-// Convert date string into a valid ISO format (inserting colon in the timezone)
-const validDateString = dateString.replace(/([+-]\d{2})(\d{2})$/, '$1:$2');
-return validDateString;
+function convertdate(dateString) {
+	// Convert date string into a valid ISO format (inserting colon in the timezone)
+	const validDateString = dateString.replace(/([+-]\d{2})(\d{2})$/, '$1:$2');
+	return validDateString;
 }
-function convertrdate(datestr)
-{
-var r=new Date(datestr);
-if (!r) r = new Date(convertdate(datestr));
-const options = {
-weekday: "long", 
-month: "long", 
-day: "numeric",
-year: "numeric"
-};
-const toptions = {
-hour: "numeric",
-minute: "numeric",
-second: "numeric",
-hour12: true
-};
-var final = r.toLocaleDateString("EN-US", options) + ", " + r.toLocaleTimeString("EN-US", toptions);
-return final;
+function convertrdate(datestr) {
+	var r=new Date(datestr);
+	if (!r) r = new Date(convertdate(datestr));
+	const options = {
+		weekday: "long", 
+		month: "long", 
+		day: "numeric",
+		year: "numeric"
+	};
+	const toptions = {
+		hour: "numeric",
+		minute: "numeric",
+		second: "numeric",
+		hour12: true
+	};
+	var final = r.toLocaleDateString("EN-US", options) + ", " + r.toLocaleTimeString("EN-US", toptions);
+	return final;
 }
