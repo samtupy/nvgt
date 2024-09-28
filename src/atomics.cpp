9c80f1a5acd2772064875520deb4b58ce937ccb8
Ethin Probst <ethindp@pm.me>
2024-09-28 07:44:05 -0500

    Added draft of atomics library

---
+++ src/atomics.cpp
@@ -0,0 +1,23 @@
+/* atomics.cpp - Integration of STL atomics
+ *
+ * NVGT - NonVisual Gaming Toolkit
+ * Copyright (c) 2022-2024 Sam Tupy
+ * https://nvgt.gg
+ * This software is provided "as-is", without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "atomics.h"
+#include <atomic>
+#include <new>
+#include <angelscript.h>
+
+static thread_local std::memory_order memory_order = std::memory_order::seq_cst;
+
+void register_atomics(IScriptEngine* engine) {
+// Atomic flag
+engine->RegisterObjectType("atomic_flag", sizeof(std::atomic_flag), asOBJ_VALUE | asOBJ_POD | asGetTypeTraits<std::atomic_flag>());
+engine->RegisterObjectBehaviour("atomic_flag", asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(std::atomic_flag::atomic_flag), asCALL_THISCALL);
