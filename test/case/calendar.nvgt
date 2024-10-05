samtupy/nvgt

commit 4b0e3f3a6c307469c6475203e4c57a936d100ce5

Form: modified add method of the control item to add only if it is ct_input, eliminated checking if it is ct_keyboard_area in void check, and modified the press of the enter key (#68)

release/include/form.nvgt
@@ -669,7 +669,7 @@ class audio_form {
 			if (subform)
 				return 2;
 		}
-		if ((focused < 0 || c_form[focused].type != ct_keyboard_area) && (focused < 0 || (c_form[focused].multiline == false) || (c_form[focused].multiline_enter == false && (key_up(KEY_LCTRL)) && (key_up(KEY_RCTRL)) && (key_up(KEY_LSHIFT)) && (key_up(KEY_RSHIFT)))) && ((key_pressed(KEY_RETURN)) || (key_pressed(KEY_NUMPAD_ENTER)) || (autotab == 3))) {
+		if ((focused < 0 || c_form[focused].type != ct_keyboard_area) && (focused < 0 || ((c_form[focused].multiline_enter && c_form[focused].multiline && c_form[focused].type == ct_input || c_form[focused].type == ct_button) && (key_up(KEY_LCTRL)) && (key_up(KEY_RCTRL)) && (key_up(KEY_LSHIFT)) && (key_up(KEY_RSHIFT)))) && ((key_pressed(KEY_RETURN)) || (key_pressed(KEY_NUMPAD_ENTER)) || (autotab == 3))) {
 			stop_speech();
 			if ((defaults == -1) && (focused > -1)) {
 				if (c_form[focused].type == ct_button || c_form[focused].type == ct_link)
@@ -2585,8 +2585,6 @@ class control {
 		return this.disallowed_chars.find(char) > -1;
 	}
 	void check(audio_form@ f, int tab_index) {
-		if (type == ct_keyboard_area)
-			return;
 		string char;
 		char = get_characters();
 		if ((type == ct_input) && key_up(KEY_LALT) && (focused)) {
@@ -3127,6 +3125,8 @@ class control {
 			speak(selection + " " + highlight_selection_speech_text, true, false);
 	}
 	void add(string character, bool silent = false) {
+		if(type != ct_input)
+		return;
 		string new_text = text;
 		if (sel_start > -1 && sel_end > -1) {
 			cursor = sel_start;

commit 6cf86407e118e0b4c3e62c87359f250f6b70f857

changelog, calendar now reference type, form fix.
* Fix form issue introduced in #68 that caused enter to not click default button in a text field.

* The calendar object is now registered as a reference type for backward compatibility, this resulted in the angelscript_refcounted_duplicating_method template to be added.

* Though it may still be updated a bit, finally composed the changelog in preparation for the next release!

release/include/form.nvgt
@@ -669,7 +669,7 @@ class audio_form {
 			if (subform)
 				return 2;
 		}
-		if ((focused < 0 || c_form[focused].type != ct_keyboard_area) && (focused < 0 || ((c_form[focused].multiline_enter && c_form[focused].multiline && c_form[focused].type == ct_input || c_form[focused].type == ct_button) && (key_up(KEY_LCTRL)) && (key_up(KEY_RCTRL)) && (key_up(KEY_LSHIFT)) && (key_up(KEY_RSHIFT)))) && ((key_pressed(KEY_RETURN)) || (key_pressed(KEY_NUMPAD_ENTER)) || (autotab == 3))) {
+		if ((focused < 0 || c_form[focused].type != ct_keyboard_area) && (focused < 0 || ((!c_form[focused].multiline_enter || !c_form[focused].multiline) && c_form[focused].type == ct_input || c_form[focused].type == ct_button) && key_up(KEY_LCTRL) && key_up(KEY_RCTRL) && key_up(KEY_LSHIFT) && key_up(KEY_RSHIFT)) && (key_pressed(KEY_RETURN) || key_pressed(KEY_NUMPAD_ENTER) || (autotab == 3))) {
 			stop_speech();
 			if ((defaults == -1) && (focused > -1)) {
 				if (c_form[focused].type == ct_button || c_form[focused].type == ct_link)

src/pocostuff.h
@@ -100,5 +100,7 @@ template <class T> inline void angelscript_refcounted_register(asIScriptEngine* 
 
 // This is a template constructor that generally stops one from needing to create factory functions for each object that uses this angelscript_refcounted mechanism.
 template <class T, typename... A> T* angelscript_refcounted_factory(A... args) { return new (angelscript_refcounted_create<T>()) T(args...); }
+// And similarly, a common trait of value typed objects is returning new versions of themselves, so handle functions that do that here too.
+template <class T, auto F, typename... A> T* angelscript_refcounted_duplicating_method(T* obj, A... args) { return new (angelscript_refcounted_create<T>()) T((obj->*F)(args...)); }
 
 void RegisterPocostuff(asIScriptEngine* engine);

src/timestuff.cpp
@@ -28,7 +28,9 @@
 #include <Poco/Timestamp.h>
 #include <Poco/Timezone.h>
 #include <SDL3/SDL.h>
+#include <scriptarray.h>
 #include "nvgt.h"
+#include "pocostuff.h" // angelscript_refcounted
 #include "scriptstuff.h"
 
 using namespace Poco;
@@ -384,7 +386,7 @@ void RegisterScriptTimestuff(asIScriptEngine* engine) {
 	engine->RegisterGlobalProperty(_O("const int64 HOURS"), (void*)&Timespan::HOURS);
 	engine->RegisterGlobalProperty(_O("const int64 DAYS"), (void*)&Timespan::DAYS);
 	engine->RegisterGlobalProperty(_O("uint64 timer_default_accuracy"), &timer_default_accuracy);
-	engine->RegisterObjectType("calendar", sizeof(LocalDateTime), asOBJ_VALUE | asGetTypeTraits<LocalDateTime>());
+	angelscript_refcounted_register<LocalDateTime>(engine, "calendar");
 	engine->RegisterObjectType("datetime", sizeof(DateTime), asOBJ_VALUE | asGetTypeTraits<DateTime>());
 	engine->RegisterObjectType("timespan", sizeof(Timespan), asOBJ_VALUE | asGetTypeTraits<Timespan>());
 	engine->RegisterObjectType("timestamp", sizeof(Timestamp), asOBJ_VALUE | asGetTypeTraits<Timestamp>());
@@ -485,12 +487,11 @@ void RegisterScriptTimestuff(asIScriptEngine* engine) {
 	engine->RegisterGlobalFunction("int datetime_days_of_month(int year, int month)", asFUNCTION(DateTime::daysOfMonth), asCALL_CDECL);
 	engine->RegisterGlobalFunction("bool datetime_is_valid(int year, int month, int day, int hour = 0, int minute = 0, int second = 0, int millisecond = 0, int microsecond = 0)", asFUNCTION(DateTime::isValid), asCALL_CDECL);
 
-	engine->RegisterObjectBehaviour("calendar", asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(timestuff_construct<LocalDateTime>), asCALL_CDECL_OBJFIRST);
-	engine->RegisterObjectBehaviour("calendar", asBEHAVE_CONSTRUCT, "void f(double julian_day)", asFUNCTION((timestuff_construct<LocalDateTime, double>)), asCALL_CDECL_OBJFIRST);
-	engine->RegisterObjectBehaviour("calendar", asBEHAVE_CONSTRUCT, "void f(int year, int month, int day, int hour = 0, int minute = 0, int second = 0, int millisecond = 0, int microsecond = 0)", asFUNCTION((timestuff_construct<LocalDateTime, int, int, int, int, int, int, int, int>)), asCALL_CDECL_OBJFIRST);
-	engine->RegisterObjectBehaviour("calendar", asBEHAVE_CONSTRUCT, "void f(const datetime&in)", asFUNCTION((timestuff_construct<LocalDateTime, const DateTime&>)), asCALL_CDECL_OBJFIRST);
-	engine->RegisterObjectBehaviour("calendar", asBEHAVE_CONSTRUCT, "void f(const calendar&in)", asFUNCTION(timestuff_copy_construct<LocalDateTime>), asCALL_CDECL_OBJFIRST);
-	engine->RegisterObjectBehaviour("calendar", asBEHAVE_DESTRUCT, "void f()", asFUNCTION(timestuff_destruct<LocalDateTime>), asCALL_CDECL_OBJFIRST);
+	engine->RegisterObjectBehaviour("calendar", asBEHAVE_FACTORY, "calendar@ f()", asFUNCTION(angelscript_refcounted_factory<LocalDateTime>), asCALL_CDECL);
+	engine->RegisterObjectBehaviour("calendar", asBEHAVE_FACTORY, "calendar@ f(double julian_day)", asFUNCTION((angelscript_refcounted_factory<LocalDateTime, double>)), asCALL_CDECL);
+	engine->RegisterObjectBehaviour("calendar", asBEHAVE_FACTORY, "calendar@ f(int year, int month, int day, int hour = 0, int minute = 0, int second = 0, int millisecond = 0, int microsecond = 0)", asFUNCTION((angelscript_refcounted_factory<LocalDateTime, int, int, int, int, int, int, int, int>)), asCALL_CDECL);
+	engine->RegisterObjectBehaviour("calendar", asBEHAVE_FACTORY, "calendar@ f(const datetime&in)", asFUNCTION((angelscript_refcounted_factory<LocalDateTime, const DateTime&>)), asCALL_CDECL);
+	engine->RegisterObjectBehaviour("calendar", asBEHAVE_FACTORY, "calendar@ f(const calendar&in)", asFUNCTION((angelscript_refcounted_factory<LocalDateTime, const LocalDateTime&>)), asCALL_CDECL);
 	engine->RegisterObjectMethod("calendar", "calendar& opAssign(const calendar&in)", asMETHODPR(LocalDateTime, operator=, (const LocalDateTime&), LocalDateTime&), asCALL_THISCALL);
 	engine->RegisterObjectMethod("calendar", "calendar& opAssign(const timestamp&in)", asMETHODPR(LocalDateTime, operator=, (const Timestamp&), LocalDateTime&), asCALL_THISCALL);
 	engine->RegisterObjectMethod("calendar", "calendar& opAssign(double julian_day)", asMETHODPR(LocalDateTime, operator=, (double), LocalDateTime&), asCALL_THISCALL);
@@ -516,8 +517,8 @@ void RegisterScriptTimestuff(asIScriptEngine* engine) {
 	engine->RegisterObjectMethod("calendar", "int64 get_UTC_time() const property", asMETHOD(LocalDateTime, utcTime), asCALL_THISCALL);
 	engine->RegisterObjectMethod("calendar", "bool opEquals(const calendar&in) const", asMETHOD(LocalDateTime, operator==), asCALL_THISCALL);
 	engine->RegisterObjectMethod("calendar", "int opCmp(const calendar&in) const", asFUNCTION((timestuff_opCmp<LocalDateTime, const LocalDateTime&>)), asCALL_CDECL_OBJFIRST);
-	engine->RegisterObjectMethod("calendar", "calendar opAdd(const timespan&in) const", asMETHODPR(LocalDateTime, operator+, (const Timespan&) const, LocalDateTime), asCALL_THISCALL);
-	engine->RegisterObjectMethod("calendar", "calendar opSub(const timespan&in) const", asMETHODPR(LocalDateTime, operator-, (const Timespan&) const, LocalDateTime), asCALL_THISCALL);
+	engine->RegisterObjectMethod("calendar", "calendar@ opAdd(const timespan&in) const", asFUNCTION((angelscript_refcounted_duplicating_method<LocalDateTime, &LocalDateTime::operator+, const Timespan&>)), asCALL_CDECL_OBJFIRST);
+	engine->RegisterObjectMethod("calendar", "calendar@ opSub(const timespan&in) const", asFUNCTION((angelscript_refcounted_duplicating_method<LocalDateTime, static_cast<LocalDateTime (LocalDateTime::*)(const Timespan&) const>(&LocalDateTime::operator-), const Timespan&>)), asCALL_CDECL_OBJFIRST);
 	engine->RegisterObjectMethod("calendar", "timespan opSub(const calendar&in) const", asMETHODPR(LocalDateTime, operator-, (const LocalDateTime&) const, Timespan), asCALL_THISCALL);
 	engine->RegisterObjectMethod("calendar", "calendar& opAddAssign(const timespan&in)", asMETHODPR(LocalDateTime, operator+=, (const Timespan&), LocalDateTime&), asCALL_THISCALL);
 	engine->RegisterObjectMethod("calendar", "calendar& opSubAssign(const timespan&in)", asMETHODPR(LocalDateTime, operator-=, (const Timespan&), LocalDateTime&), asCALL_THISCALL);

test/case/calendar.nvgt
@@ -1,0 +1,22 @@
+void mod_calendar(calendar@ c, int seconds) {
+	c += timespan(seconds, 0);
+}
+
+void test_calendar() {
+	calendar@ c = calendar();
+	assert(c.year == DATE_YEAR);
+	int m = c.minute;
+	mod_calendar(c, 120);
+	calendar c2;
+	assert(m + 2 == c.minute or c.minute < 2);
+	assert (c2 < c);
+	c.set(2020, 1, 1, 0, 0, 0);
+	assert (c.year == 2020 and c.hour == 0);
+	c = c2;
+	mod_calendar(c, -240);
+	assert (c < c2);
+	@c = c2;
+	assert (c == c2);
+	c2 += 10;
+	assert (c == c2);
+}

test/interact/formtest.nvgt
@@ -6,7 +6,7 @@
 void main() {
 	audio_form f;
 	f.create_window("test");
-	int b = f.create_input_box("type text here", "or leave it alone");
+	int b = f.create_input_box("type text here", "or leave it alone", multiline: true, multiline_enter: false);
 	//Disallow characters field
 	int c = f.create_input_box("Text without spaces and quotes");
 	f.set_disallowed_chars(c, " \"", false, "Spaces and quotes are not allowed");