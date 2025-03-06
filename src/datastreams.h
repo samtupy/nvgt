302f1325d1c3ba5d94034f2d297aa459bf1c0db6
Sam Tupy <webmaster@samtupy.com>
2025-03-06 14:29:03 -0600

    add 7bit integer encoding to datastreams

--- src/datastreams.h
+++ src/datastreams.h
@@ -72,6 +72,9 @@ public:
 	unsigned int write(const std::string& data);
 	template<typename T> datastream& read(T& value);
 	template <typename T> T read();
+	unsigned long long read_7bit_encoded();
+	void read_7bit_encoded(unsigned long long& integer);
+	void write_7bit_encoded(unsigned long long integer);
 	template<typename T> datastream& write(T value);
 	std::string read_until(const std::string& text, bool full);
 	inline bool good() {
