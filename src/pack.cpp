907b40227e14ce373cab849e973b8c7b6bed3456
Ivan S <ivan@ims-productions.com>
2025-04-16 14:39:12 -0500

    Updated bool set_sound_storage function in bgt_compat. Removed bool set_sound_decryption_key as it was misleading and always returned true. You can now optionally provide a key in set_sound_storage and force the encryption. Registered pack_file.pack_name property.

--- src/pack.cpp
+++ src/pack.cpp
@@ -783,6 +783,7 @@ void RegisterScriptPack(asIScriptEngine *engine)
 	engine->RegisterObjectMethod("pack_file", "bool file_exists(const string &in filename)", asMETHOD(pack, file_exists), asCALL_THISCALL);
 	engine->RegisterObjectMethod("pack_file", "int64 get_file_size(const string &in filename)", asMETHOD(pack, get_file_size), asCALL_THISCALL);
 	engine->RegisterObjectMethod("pack_file", "datastream @get_file(const string &in filename, const string &in encoding = \"\", int byteorder = STREAM_BYTE_ORDER_NATIVE)", asMETHOD(pack, get_file_script), asCALL_THISCALL);
+	engine->RegisterObjectMethod("pack_file", "string get_pack_name() const property", asMETHOD(pack, get_pack_name), asCALL_THISCALL);
 	engine->RegisterObjectMethod("pack_file", "bool get_active() const property", asMETHOD(pack, get_active), asCALL_THISCALL);
 	engine->RegisterObjectMethod("pack_file", "int64 get_file_count() const property", asMETHOD(pack, get_file_count), asCALL_THISCALL);
 	engine->RegisterObjectMethod("pack_file", "string[]@ list_files() const", asMETHOD(pack, list_files), asCALL_THISCALL);
