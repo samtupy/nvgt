df13cebd757833102f9f81fd5ffcd8d585f2f083
Ethin Probst <ethindp@pm.me>
2025-05-14 21:53:31 -0500

    Fixed GTK crashing bugs, added test for idle_ticks

--- src/posix.cpp
+++ src/posix.cpp
@@ -52,24 +52,30 @@ static void on_close(GtkButton*, gpointer ud) {
 	g_return_val_if_fail( (!parent) || GTK_IS_WINDOW(parent),	"" );
 	g_return_val_if_fail( !title.empty(),						"" );
 	g_return_val_if_fail( !prompt.empty(),					   "" );
+	if (!gtk_init_check()) return "";
 	GtkWindow* win = GTK_WINDOW(gtk_window_new());
+	assert(win);
 	gtk_window_set_title	 (win, title.c_str());
 	gtk_window_set_modal	 (win, TRUE);
 	gtk_window_set_transient_for(win, parent);
 	gtk_window_set_destroy_with_parent(win, TRUE);
 	auto *vbox = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 8));
+	assert(vbox);
 	gtk_widget_set_margin_top   (GTK_WIDGET(vbox),  12);
 	gtk_widget_set_margin_bottom(GTK_WIDGET(vbox),  12);
 	gtk_widget_set_margin_start (GTK_WIDGET(vbox),  12);
 	gtk_widget_set_margin_end   (GTK_WIDGET(vbox),  12);
 	gtk_window_set_child(win, GTK_WIDGET(vbox));
 	auto *lbl = GTK_LABEL(gtk_label_new(prompt.c_str()));
+	assert(lbl);
 	gtk_label_set_xalign(lbl, 0.0);
 	gtk_label_set_wrap  (lbl, TRUE);
 	gtk_box_append	  (vbox, GTK_WIDGET(lbl));
 	auto *ent = GTK_ENTRY(gtk_entry_new());
+	assert(ent);
 	gtk_entry_set_max_length	   (ent, 0);
 	auto* buffer = gtk_entry_get_buffer(ent);
+	assert(buffer);
 	gtk_entry_buffer_set_text(buffer, default_text.c_str(), default_text.size());
 	auto hints = static_cast<GtkInputHints>(GTK_INPUT_HINT_SPELLCHECK | GTK_INPUT_HINT_WORD_COMPLETION | GTK_INPUT_HINT_EMOJI);
 	if (secure) {
@@ -80,15 +86,18 @@ static void on_close(GtkButton*, gpointer ud) {
 	gtk_entry_set_activates_default(ent, TRUE);
 	gtk_box_append				 (vbox, GTK_WIDGET(ent));
 	auto *b_ok = GTK_BUTTON(gtk_button_new_with_mnemonic("_OK"));
+	assert(b_ok);
 	gtk_widget_set_can_focus (GTK_WIDGET(b_ok), TRUE);
 	gtk_window_set_default_widget (GTK_WINDOW(win), GTK_WIDGET(b_ok));
 	gtk_widget_add_css_class (GTK_WIDGET(b_ok), "suggested-action");
 	gtk_box_append			(vbox, GTK_WIDGET(b_ok));
 	auto *b_cancel = GTK_BUTTON(gtk_button_new_with_mnemonic("_Cancel"));
+	assert(b_cancel);
 	gtk_widget_set_can_focus   (GTK_WIDGET(b_cancel), TRUE);
 	gtk_box_append			  (vbox, GTK_WIDGET(b_cancel));
 	input_data data;
 	data.loop  = g_main_loop_new(nullptr, FALSE);
+	assert(data.loop );
 	data.win   = win;
 	data.entry = ent;
 	g_signal_connect(b_ok,	 "clicked", G_CALLBACK(on_ok),	 &data);
@@ -106,35 +115,44 @@ static void on_close(GtkButton*, gpointer ud) {
 	g_return_val_if_fail( !title.empty(),					   FALSE );
 	g_return_val_if_fail( !prompt.empty(),					 FALSE );
 	g_return_val_if_fail( !text.empty(),					   FALSE );
+	if (!gtk_init_check()) return "";
 	GtkWindow* win = GTK_WINDOW(gtk_window_new());
+	assert(win);
 	gtk_window_set_title	 (win, title.c_str());
 	gtk_window_set_modal	 (win, TRUE);
 	gtk_window_set_transient_for(win, parent);
 	gtk_window_set_destroy_with_parent(win, TRUE);
 	auto *vbox = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 8));
+	assert(vbox);
 	gtk_widget_set_margin_top   (GTK_WIDGET(vbox),  12);
 	gtk_widget_set_margin_bottom(GTK_WIDGET(vbox),  12);
 	gtk_widget_set_margin_start (GTK_WIDGET(vbox),  12);
 	gtk_widget_set_margin_end   (GTK_WIDGET(vbox),  12);
 	gtk_window_set_child(win, GTK_WIDGET(vbox));
 	auto *lbl = GTK_LABEL(gtk_label_new(prompt.c_str()));
+	assert(lbl);
 	gtk_label_set_xalign(lbl, 0.0);
 	gtk_label_set_wrap  (lbl, TRUE);
 	gtk_box_append	  (vbox, GTK_WIDGET(lbl));
 	auto *sw = GTK_SCROLLED_WINDOW(gtk_scrolled_window_new());
+	assert(sw);
 	gtk_scrolled_window_set_policy(sw, GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 	auto *tv = GTK_TEXT_VIEW(gtk_text_view_new());
+	assert(tv);
 	gtk_text_view_set_editable	  (tv, FALSE);
 	gtk_text_view_set_cursor_visible(tv, FALSE);
 	gtk_scrolled_window_set_child   (sw, GTK_WIDGET(tv));
 	auto *buf = gtk_text_view_get_buffer(tv);
+	assert(buf);
 	gtk_text_buffer_set_text(buf, text.c_str(), text.size());
 	gtk_box_append(vbox, GTK_WIDGET(sw));
 	auto *b_close = GTK_BUTTON(gtk_button_new_with_mnemonic("_Close"));
+	assert(b_close);
 	gtk_widget_set_can_focus(GTK_WIDGET(b_close), TRUE);
 	gtk_box_append(vbox, GTK_WIDGET(b_close));
 	info_data data;
 	data.loop = g_main_loop_new(nullptr, FALSE);
+	assert(data.loop);
 	data.win  = win;
 	g_signal_connect(b_close, "clicked", G_CALLBACK(on_close), &data);
 	gtk_widget_set_visible(GTK_WIDGET(vbox), TRUE);
@@ -151,12 +169,12 @@ void *nvgt_dlopen(const char *lib_name) {
 auto* ptr = dlopen(lib_name, RTLD_NOW | RTLD_DEEPBIND);
 if (!ptr)
 if (isatty(fileno(stderr))) {
-std::cerr << "Aborting: library loader could not load " << lib_name << "\n";
-std::exit(1);
+std::cerr << "Error: library loader could not load " << lib_name << "\n";
+return nullptr;
 } else {
-const auto msg = Poco::format("Aborting: library loader could not load %s", std::string(lib_name));
-SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Critical error", msg.c_str(), nullptr);
-std::exit(1);
+const auto msg = Poco::format("Library loader could not load %s", std::string(lib_name));
+SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", msg.c_str(), nullptr);
+return nullptr;
 }
 return ptr;
 }
@@ -173,12 +191,12 @@ libname.clear();
 }
 libname = lm->l_name;
 if (isatty(fileno(stderr))) {
-std::cerr << Poco::format("Aborting: library loader could not find symbol %s in library %s\n", std::string(sym_name), libname.empty() ? "NVGT core" : std::string(libname));
-std::exit(1);
+std::cerr << Poco::format("Error: library loader could not find symbol %s in library %s\n", std::string(sym_name), libname.empty() ? "NVGT core" : std::string(libname));
+return nullptr;
 } else {
-const auto msg = Poco::format("Aborting: library loader could not find symbol %s in library %s", sym_name, libname.empty() ? "NVGT core" : std::string(libname));
-SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Critical error", msg.c_str(), nullptr);
-std::exit(1);
+const auto msg = Poco::format("Library loader could not find symbol %s in library %s", sym_name, libname.empty() ? "NVGT core" : std::string(libname));
+SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", msg.c_str(), nullptr);
+return nullptr;
 }
 }
 return ptr;
