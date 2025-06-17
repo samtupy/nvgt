2cd093e80d21129b95ee1a39c695a613b3fc08c3
Amerikranian <olegjpittman@gmail.com>
2025-06-17 04:30:29 -0500

    Fix negative indices in strings

--- ASAddon/src/scriptstdstring_utils.cpp
+++ ASAddon/src/scriptstdstring_utils.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 BEGIN_AS_NAMESPACE
 
-static asITypeInfo *StringArrayType = NULL;
+static asITypeInfo* StringArrayType = NULL;
 // This function takes an input string and splits it into parts by looking
 // for a specified delimiter. Example:
 //
@@ -23,74 +23,61 @@ static asITypeInfo *StringArrayType = NULL;
 //
 // AngelScript signature:
 // array<string>@ string::split(const string &in delim, bool full = true) const
-static CScriptArray *StringSplit(const string &delim, bool full, bool allow_blanks, const string &str)
-{
+static CScriptArray* StringSplit(const string &delim, bool full, bool allow_blanks, const string &str) {
 	// Obtain a pointer to the engine
 	asIScriptContext *ctx = asGetActiveContext();
 	asIScriptEngine *engine = ctx->GetEngine();
-
 	// TODO: This assumes that CScriptArray was already registered
 	if (!StringArrayType)
 		StringArrayType = engine->GetTypeInfoByDecl("array<string>");
-
 	// Create the array object
 	CScriptArray *array = CScriptArray::Create(StringArrayType);
-	if (delim == "")
-	{
-		array->InsertLast((void *)&str);
+	if (delim == "") {
+		array->InsertLast((void*)&str);
 		return array;
 	}
 	asUINT reserved = str.size() * 0.05; // Sam: Not sure how to predict how many elements we should initially reserve as each string could be different. Does it matter?
 	array->Reserve(reserved);
-
 	// Find the existence of the delimiter in the input string
 	size_t pos = 0, prev = 0;
 	asUINT count = 0;
-	while ((pos = (full ? str.find(delim, prev) : str.find_first_of(delim, prev))) != string::npos)
-	{
-		if (count > reserved)
-		{
+	while ((pos = (full ? str.find(delim, prev) : str.find_first_of(delim, prev))) != string::npos) {
+		if (count > reserved) {
 			reserved *= 8;
 			array->Reserve(reserved);
 		}
 		// Add the part to the array
-		if (allow_blanks || pos - prev > 0)
-		{
+		if (allow_blanks || pos - prev > 0) {
 			array->Resize(array->GetSize() + 1);
-			((string *)array->At(count))->assign(&str[prev], pos - prev);
+			((string*)array->At(count))->assign(&str[prev], pos - prev);
 			count++;
 		}
 		// Find the next part
 		if (full)
 			prev = pos + delim.length();
-		else
-		{
+		else {
 			prev = pos;
 			while (pos + delim.size() - prev > 0 && delim.find_first_of(str.substr(prev, pos + delim.size() - prev)) != string::npos)
 				prev++;
 		}
 	}
-
 	// Add the remaining part if needed
-	if (array->GetSize() < 1 || str.size() - prev > 0)
-	{
+	if (array->GetSize() < 1 || str.size() - prev > 0) {
 		array->Resize(array->GetSize() + 1);
-		((string *)array->At(count))->assign(&str[prev], str.size() - prev);
+		((string*)array->At(count))->assign(&str[prev], str.size() - prev);
 	}
 	array->Resize(array->GetSize());
 	return array;
 }
 
-static void StringSplit_Generic(asIScriptGeneric *gen)
-{
+static void StringSplit_Generic(asIScriptGeneric *gen) {
 	// Get the arguments
-	string *str = (string *)gen->GetObject();
-	string *delim = *(string **)gen->GetAddressOfArg(0);
+	string *str = (string*)gen->GetObject();
+	string *delim = *(string**)gen->GetAddressOfArg(0);
 	bool full = gen->GetArgByte(1);
 	bool allow_blanks = gen->GetArgByte(2);
-
 	// Return the array by handle
-	*(CScriptArray **)gen->GetAddressOfReturnLocation() = StringSplit(*delim, full, allow_blanks, *str);
+	*(CScriptArray**)gen->GetAddressOfReturnLocation() = StringSplit(*delim, full, allow_blanks, *str);
 }
 
 // This function takes as input an array of string handles as well as a
@@ -106,32 +93,25 @@ static void StringSplit_Generic(asIScriptGeneric *gen)
 //
 // AngelScript signature:
 // string join(const array<string> &in array, const string &in delim)
-static string StringJoin(const CScriptArray &array, const string &delim)
-{
+static string StringJoin(const CScriptArray &array, const string &delim) {
 	// Create the new string
 	string str = "";
-	if (array.GetSize())
-	{
+	if (array.GetSize()) {
 		int n;
-		for (n = 0; n < (int)array.GetSize() - 1; n++)
-		{
-			str += *(string *)array.At(n);
+		for (n = 0; n < (int)array.GetSize() - 1; n++) {
+			str += *(string*)array.At(n);
 			str += delim;
 		}
-
 		// Add the last part
-		str += *(string *)array.At(n);
+		str += *(string*)array.At(n);
 	}
-
 	return str;
 }
 
-static void StringJoin_Generic(asIScriptGeneric *gen)
-{
+static void StringJoin_Generic(asIScriptGeneric *gen) {
 	// Get the arguments
-	CScriptArray *array = *(CScriptArray **)gen->GetAddressOfArg(0);
-	string *delim = *(string **)gen->GetAddressOfArg(1);
-
+	CScriptArray *array = *(CScriptArray**)gen->GetAddressOfArg(0);
+	string *delim = *(string**)gen->GetAddressOfArg(1);
 	// Return the string
 	new (gen->GetAddressOfReturnLocation()) string(StringJoin(*array, *delim));
 }
@@ -143,13 +123,12 @@ static void StringJoin_Generic(asIScriptGeneric *gen)
 //
 // AngelScript signature:
 // string string::slice(int start = 0, int end = 0) const
-static string StringSlice(int start, int end, const string &str)
-{
+static string StringSlice(int start, int end, const string &str) {
 	asUINT size = str.size();
 	if (start < 0)
 		start = size + start;
-	if (end <= 0)
-		end = size - end;
+	if (end < 0)
+		end = size + end;
 	if (end >= size)
 		end = size;
 	int count = end - start;
@@ -162,8 +141,7 @@ static string StringSlice(int start, int end, const string &str)
 //
 // AngelScript signature:
 // string string::replaceRange(uint start, int count, string& replace) const
-static string StringReplaceRange(asUINT start, int count, const string &replace, const string &str)
-{
+static string StringReplaceRange(asUINT start, int count, const string &replace, const string &str) {
 	if (start >= str.size() || count < 1)
 		return str;
 	// Recreate the string so that the original stays in tact.
@@ -178,15 +156,13 @@ static string StringReplaceRange(asUINT start, int count, const string &replace,
 //
 // AngelScript signature:
 // string string::replace(string& search, string& replace) const
-static string StringReplace(const string &search, const string &replace, bool replaceAll, asUINT offset, const string &str)
-{
+static string StringReplace(const string &search, const string &replace, bool replaceAll, asUINT offset, const string &str) {
 	if (search == "" || str == "")
 		return str;
 	string ret = str;
 	int replacementCount = 0;
 	size_t pos = ret.find(search, offset);
-	while ((pos = ret.find(search, pos)) != string::npos)
-	{
+	while ((pos = ret.find(search, pos)) != string::npos) {
 		ret.replace(pos, search.length(), replace);
 		pos += replace.length();
 		replacementCount += 1;
@@ -196,14 +172,12 @@ static string StringReplace(const string &search, const string &replace, bool re
 	return ret;
 }
 // Like above, but does so in place.
-static string &StringReplaceThis(const string &search, const string &replace, bool replaceAll, asUINT offset, string &str)
-{
+static string& StringReplaceThis(const string &search, const string &replace, bool replaceAll, asUINT offset, string &str) {
 	if (search == "" || str == "")
 		return str;
 	int replacementCount = 0;
 	size_t pos = str.find(search, offset);
-	while ((pos = str.find(search, pos)) != string::npos)
-	{
+	while ((pos = str.find(search, pos)) != string::npos) {
 		str.replace(pos, search.length(), replace);
 		pos += replace.length();
 		replacementCount += 1;
@@ -213,42 +187,37 @@ static string &StringReplaceThis(const string &search, const string &replace, bo
 	return str;
 }
 
-static void StringSlice_Generic(asIScriptGeneric *gen)
-{
+static void StringSlice_Generic(asIScriptGeneric *gen) {
 	// Get the arguments
-	string *str = (string *)gen->GetObject();
-	int start = *(int *)gen->GetAddressOfArg(0);
-	int end = *(int *)gen->GetAddressOfArg(1);
-
+	string *str = (string*)gen->GetObject();
+	int start = *(int*)gen->GetAddressOfArg(0);
+	int end = *(int*)gen->GetAddressOfArg(1);
 	// Return the substring
 	new (gen->GetAddressOfReturnLocation()) string(StringSlice(start, end, *str));
 }
 
-static void StringReplaceRange_Generic(asIScriptGeneric *gen)
-{
+static void StringReplaceRange_Generic(asIScriptGeneric *gen) {
 	asUINT start = gen->GetArgDWord(1);
 	int count = gen->GetArgDWord(2);
-	string *replace = reinterpret_cast<string *>(gen->GetArgAddress(3));
-	string *self = reinterpret_cast<string *>(gen->GetObject());
-	*reinterpret_cast<string *>(gen->GetAddressOfReturnLocation()) = StringReplaceRange(start, count, *replace, *self);
+	string *replace = reinterpret_cast<string*>(gen->GetArgAddress(3));
+	string *self = reinterpret_cast<string*>(gen->GetObject());
+	*reinterpret_cast<string*>(gen->GetAddressOfReturnLocation()) = StringReplaceRange(start, count, *replace, *self);
 }
 
-static void StringReplace_Generic(asIScriptGeneric *gen)
-{
-	string *search = reinterpret_cast<string *>(gen->GetArgAddress(1));
-	string *replace = reinterpret_cast<string *>(gen->GetArgAddress(2));
+static void StringReplace_Generic(asIScriptGeneric *gen) {
+	string *search = reinterpret_cast<string*>(gen->GetArgAddress(1));
+	string *replace = reinterpret_cast<string*>(gen->GetArgAddress(2));
 	bool replaceAll = gen->GetArgByte(3);
 	asUINT offset = gen->GetArgDWord(4);
-	string *self = reinterpret_cast<string *>(gen->GetObject());
-	*reinterpret_cast<string *>(gen->GetAddressOfReturnLocation()) = StringReplace(*search, *replace, replaceAll, offset, *self);
+	string *self = reinterpret_cast<string*>(gen->GetObject());
+	*reinterpret_cast<string*>(gen->GetAddressOfReturnLocation()) = StringReplace(*search, *replace, replaceAll, offset, *self);
 }
 
 // Returns a reversed version of the given string.
 //
 // AngelScript signature:
 // string string::reverse() const
-static string StringReverse(const string &str)
-{
+static string StringReverse(const string &str) {
 	string ret(&str[0], str.size());
 	std::reverse(ret.begin(), ret.end());
 	return ret;
@@ -257,8 +226,7 @@ static string StringReverse(const string &str)
 //
 // AngelScript signature:
 // string string::lower() const
-static string StringLower(const string &str)
-{
+static string StringLower(const string &str) {
 	string ret;
 	ret.reserve(str.size());
 	for (int i = 0; i < str.size(); i++)
@@ -270,8 +238,7 @@ static string StringLower(const string &str)
 //
 // AngelScript signature:
 // string string::lower() const
-static string StringUpper(const string &str)
-{
+static string StringUpper(const string &str) {
 	string ret;
 	ret.reserve(str.size());
 	for (int i = 0; i < str.size(); i++)
@@ -283,10 +250,8 @@ static string StringUpper(const string &str)
 //
 // AngelScript signature:
 // bool string::isLower() const
-static bool StringIsLower(const string &str)
-{
-	for (int i = 0; i < str.size(); i++)
-	{
+static bool StringIsLower(const string &str) {
+	for (int i = 0; i < str.size(); i++) {
 		if (!islower(str[i]))
 			return false;
 	}
@@ -297,10 +262,8 @@ static bool StringIsLower(const string &str)
 //
 // AngelScript signature:
 // bool string::isUpper() const
-static bool StringIsUpper(const string &str)
-{
-	for (int i = 0; i < str.size(); i++)
-	{
+static bool StringIsUpper(const string &str) {
+	for (int i = 0; i < str.size(); i++) {
 		if (!isupper(str[i]))
 			return false;
 	}
@@ -311,10 +274,8 @@ static bool StringIsUpper(const string &str)
 //
 // AngelScript signature:
 // bool string::isPunct() const
-static bool StringIsPunct(const string &str)
-{
-	for (int i = 0; i < str.size(); i++)
-	{
+static bool StringIsPunct(const string &str) {
+	for (int i = 0; i < str.size(); i++) {
 		if (!ispunct(str[i]))
 			return false;
 	}
@@ -325,12 +286,10 @@ static bool StringIsPunct(const string &str)
 //
 // AngelScript signature:
 // bool string::isDigit() const
-static bool StringIsDigit(const string &str)
-{
+static bool StringIsDigit(const string &str) {
 	if (str.size() < 1)
 		return false;
-	for (int i = 0; i < str.size(); i++)
-	{
+	for (int i = 0; i < str.size(); i++) {
 		if (!isdigit(str[i]))
 			return false;
 	}
@@ -341,10 +300,8 @@ static bool StringIsDigit(const string &str)
 //
 // AngelScript signature:
 // bool string::isAlpha() const
-static bool StringIsAlpha(const string &str)
-{
-	for (int i = 0; i < str.size(); i++)
-	{
+static bool StringIsAlpha(const string &str) {
+	for (int i = 0; i < str.size(); i++) {
 		if (!isalpha(str[i]))
 			return false;
 	}
@@ -355,18 +312,15 @@ static bool StringIsAlpha(const string &str)
 //
 // AngelScript signature:
 // bool string::isAlphanum() const
-static bool StringIsAlphanum(const string &str)
-{
-	for (int i = 0; i < str.size(); i++)
-	{
+static bool StringIsAlphanum(const string &str) {
+	for (int i = 0; i < str.size(); i++) {
 		if (!isalpha(str[i]) && !isdigit(str[i]))
 			return false;
 	}
 	return true;
 }
 
-static string string_multiply(const string &str, asUINT multiplier)
-{
+static string string_multiply(const string &str, asUINT multiplier) {
 	if (multiplier == 0)
 		return "";
 	else if (multiplier == 1)
@@ -377,15 +331,12 @@ static string string_multiply(const string &str, asUINT multiplier)
 		result += str;
 	return result;
 }
-static string &string_multiply_assign(string &str, asUINT multiplier)
-{
-	if (multiplier == 0)
-	{
+static string& string_multiply_assign(string &str, asUINT multiplier) {
+	if (multiplier == 0) {
 		str = "";
 		str.shrink_to_fit();
 		return str;
-	}
-	else if (multiplier == 1)
+	} else if (multiplier == 1)
 		return str;
 	str.reserve(str.size() * multiplier);
 	string tmp = str;
@@ -393,13 +344,11 @@ static string &string_multiply_assign(string &str, asUINT multiplier)
 		str += tmp;
 	return str;
 }
-static asQWORD string_count(string &str, const string &search, asQWORD start)
-{
+static asQWORD string_count(string &str, const string &search, asQWORD start) {
 	if (str.empty() || search.empty())
 		return 0;
 	asUINT c = 0;
-	while (true)
-	{
+	while (true) {
 		size_t pos = str.find(search, start);
 		if (pos == std::string::npos)
 			break;
@@ -411,12 +360,9 @@ static asQWORD string_count(string &str, const string &search, asQWORD start)
 
 // This is where the utility functions are registered.
 // The string type must have been registered first.
-void RegisterStdStringUtils(asIScriptEngine *engine)
-{
+void RegisterStdStringUtils(asIScriptEngine *engine) {
 	int r;
-
-	if (strstr(asGetLibraryOptions(), "AS_MAX_PORTABILITY"))
-	{
+	if (strstr(asGetLibraryOptions(), "AS_MAX_PORTABILITY")) {
 		r = engine->RegisterObjectMethod("string", "array<string>@ split(const string &in, bool = true, bool=false) const", asFUNCTION(StringSplit_Generic), asCALL_GENERIC);
 		assert(r >= 0);
 		r = engine->RegisterGlobalFunction("string join(const array<string> &in, const string &in)", asFUNCTION(StringJoin_Generic), asCALL_GENERIC);
@@ -427,9 +373,7 @@ void RegisterStdStringUtils(asIScriptEngine *engine)
 		assert(r >= 0);
 		r = engine->RegisterObjectMethod("string", "string replace(const string& in, const string& in, bool = true, uint = 0) const", asFUNCTION(StringReplace_Generic), asCALL_GENERIC);
 		assert(r >= 0);
-	}
-	else
-	{
+	} else {
 		r = engine->RegisterObjectMethod("string", "array<string>@ split(const string &in, bool = true, bool=false) const", asFUNCTION(StringSplit), asCALL_CDECL_OBJLAST);
 		assert(r >= 0);
 		r = engine->RegisterGlobalFunction("string join(const array<string> &in, const string &in)", asFUNCTION(StringJoin), asCALL_CDECL);
