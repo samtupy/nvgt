0497f7b17f643a8bf3fdfcbd6cfb694700dbefc8
Sam Tupy <webmaster@samtupy.com>
2025-07-04 11:59:10 -0500

    vcpkg work

--- vcpkg/triplets/arm64-osx.cmake
+++ vcpkg/triplets/arm64-osx.cmake
@@ -1,6 +1,10 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+if(PORT MATCHES "libgit" OR PORT MATCHES "libplist" OR PORT MATCHES "libarchive")
+	set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+	set(VCPKG_LIBRARY_LINKAGE static)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 if(PORT MATCHES "openssl" OR PORT MATCHES "libffi")
