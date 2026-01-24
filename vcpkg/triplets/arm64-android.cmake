9da4852aecd7ffc427f5a8873d3809579ab6720d
Sam Tupy <webmaster@samtupy.com>
2026-01-24 17:41:20 -0600

    oops? We want dynamic not shared for Android triplet

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -1,5 +1,5 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
-set(VCPKG_CRT_LINKAGE shared)
+set(VCPKG_CRT_LINKAGE dynamic)
 if(PORT STREQUAL "sdl3" OR PORT MATCHES "libarchive" OR PORT MATCHES "libgit2")
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
