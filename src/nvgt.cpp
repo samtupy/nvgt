0cc4778ab6293b172f0dfeac222eb62a7225465d
NKPR0d <kolokolchicov.n@gmail.com>
2026-03-22 16:16:56 +0300

    Move everything to the build system, fix compiling

--- src/nvgt.cpp
+++ src/nvgt.cpp
@@ -15,9 +15,6 @@
 #include <sstream>
 #define SDL_MAIN_HANDLED // We do actually use SDL_main, but see below near the bottom of the file.
 #ifdef _WIN32
-#pragma comment(linker, "\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 	#define NOMINMAX
 	#include <windows.h>
 	#include <locale.h>
